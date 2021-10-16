aa4697ca17a8fa6f128d93ea1c25ec5a1d6c5eec
WoodDivision <ebenzaer@gmail.com>
2021-10-16 09:35:26 -0700

    add select in CityStorageInterface

--- src/interface.php
+++ src/interface.php
@@ -4,6 +4,7 @@ interface CityStorageInteface
 {
     public function insert(string $city): int;
     public function issetCity(string $city): bool;
+    public function select (string $city): int;
 }
 
 interface UserStorageInteface
